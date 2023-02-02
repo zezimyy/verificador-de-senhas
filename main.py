7df938868ce26ffa4cdc96f3060a726dc0a13d30
André Luis <66038323+zezimyy@users.noreply.github.com>
2023-02-02 02:35:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+senha = "w79CbzDojTEr6Gsx"
+
+while True:
+  inputSenha = input("digite a sua senha: ")
+
+  if inputSenha == senha:
+    print()
+    print("Senha correta!")
+    print()
+    break
+
+  else:
+    print()
+    print("Algo de errado ocorreu, tente novamente: ")
+    print()
+    
